0c65671ab5752d4fd4eb300456287ad82bc81479
Jen Caza <jencaza@gmail.com>
2022-03-11 17:00:17 -0500

    add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,41 @@
 module.exports = {
-    siteMetadata: {
-      title: `new`,
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    title: `Gatsby Blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+    author: `@jencaza33`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
     },
-    plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-reading-time`,
+        ],
+      }
+    },
+    `gatsby-trasnformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
 
-    ]
-}
+      }
+    }
+  ]
+};
